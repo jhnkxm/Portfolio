60b45804e6cf5b655777b47904444f34901554b8
jhnkxm <jhnkim@outlook.kr>
2021-10-08 16:12:58 +0900

    Javascript: Transparent home

--- main.js
+++ main.js
@@ -35,3 +35,11 @@ function scrollIntoView(selector) {
   const scrollTarget = document.querySelector(selector);
   scrollTarget.scrollIntoView({ behavior: "smooth" });
 }
+
+// Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector(".home__container");
+const homeHeight = home.getBoundingClientRect().height;
+
+document.addEventListener("scroll", () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
