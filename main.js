01b4720db006dfb6e54dc6edab9d381dd4f644d3
jhnkxm <jhnkim@outlook.kr>
2021-10-08 15:47:05 +0900

    Javascript: Navbar menu click

--- main.js
+++ main.js
@@ -11,3 +11,24 @@ document.addEventListener("scroll", () => {
     navbar.classList.remove("navbar--dark");
   }
 });
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector(".navbar__menu");
+const about = document.querySelector("#about");
+const contactMe = document.querySelector("#contact");
+const contactBtn = document.querySelector(".home__contact");
+
+navbarMenu.addEventListener("click", (event) => {
+  console.log(event.target.dataset.link);
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  const scrollTarget = document.querySelector(link);
+  scrollTarget.scrollIntoView({ behavior: "smooth" });
+});
+
+contactBtn.addEventListener("click", () => {
+  contactMe.scrollIntoView({ behavior: "smooth" });
+});
